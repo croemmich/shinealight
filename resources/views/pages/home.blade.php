3f401426cc398fbf15a93cb828901ae73cc09a14
Chris Roemmich <croemmich@myriadmobile.com>
2016-02-15 12:34:15 -0600

    add event details

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -21,6 +21,10 @@
                 of love and support. We stand together to mourn the losses faced by the Moszer family. We also stand beside
                 Michelle Schumacher who lost her husband and has to move on wading through a very difficult time of loss and pain.
                 If you choose to shine a light, or see one at someone’s door, remember the Moszer family, remember the Schumacher family.</p>
+            <p>EVENT DETAILS<br/>
+                When: February 21st, 2016 for 53 hours<br/>
+                Event Link: <a href="https://www.facebook.com/events/570614369762890/">https://www.facebook.com/events/570614369762890/</a>
+            </p>
         </div>
     </div>
 </div>
