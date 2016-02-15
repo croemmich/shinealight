4f72a6254571f9921bd05e01ea9c2aef25a1c0c1
Chris Roemmich <croemmich@myriadmobile.com>
2016-02-15 14:39:16 -0600

    couple small changes to the home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -14,16 +14,21 @@
     </div>
     <div class="row">
         <div class="col-xs-12 lj-title-paragraph">
-            <p>Wednesday, February 10th was a day that forever changed Fargo. As many of you know, a domestic disturbance
+            <p>Wednesday, February 10th was a day that forever changed Fargo. As many of you know, a domestic
+                disturbance
                 call escalated into a horrific series of events that left two dead in the wake of tragedy. Like you,
                 the people of our neighborhood mourn the tragic losses of that night. We wanted to show our support for
-                those affected by this tragedy and, as a neighborhood, felt this simple act could be a powerful expression
-                of love and support. We stand together to mourn the losses faced by the Moszer family. We also stand beside
-                Michelle Schumacher who lost her husband and has to move on wading through a very difficult time of loss and pain.
-                If you choose to shine a light, or see one at someone’s door, remember the Moszer family, remember the Schumacher family.</p>
-            <p>EVENT DETAILS<br/>
-                When: February 21st, 2016 for 53 hours<br/>
-                Event Link: <a href="https://www.facebook.com/events/570614369762890/">https://www.facebook.com/events/570614369762890/</a>
+                those affected by this tragedy and, as a neighborhood, felt this simple act could be a powerful
+                expression
+                of love and support. </p>
+            <p>We stand together to mourn the losses faced by the Moszer family. We also stand beside
+                Michelle Schumacher who lost her husband and has to move on wading through a very difficult time of loss
+                and pain.
+                If you choose to shine a light, or see one at someone’s door, remember the Moszer family, remember the
+                Schumacher family.</p>
+            <p><strong>EVENT DETAILS</strong><br/>
+                <strong>When:</strong> February 21st, 2016 for 53 hours<br/>
+                <strong>Event Link:</strong> <a href="https://www.facebook.com/events/570614369762890/">https://www.facebook.com/events/570614369762890/</a>
             </p>
         </div>
     </div>
